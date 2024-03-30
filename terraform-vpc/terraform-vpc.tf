b534702805e5c16d7f04a610b2bb4dab048e0c09
Rakshya Humagain <135014386+Rakshya05@users.noreply.github.com>
2024-03-30 17:34:46 +0545

    second commit

--- terraform-vpc/terraform-vpc.tf
+++ terraform-vpc/terraform-vpc.tf
@@ -1,4 +1,3 @@
-
 # Terraform | Provider | Version
 terraform {
   required_providers {
@@ -18,22 +17,22 @@ resource "aws_vpc" "demo-vpc" {
 }
 
 # Creating Public Subnet
-resource "aws_subnet" "demo-public-subnet" {
-  vpc_id            = aws_vpc.demo-vpc
+resource "aws_subnet" "demo-public_subent_01" {
+  vpc_id            = aws_vpc.demo-vpc.id
   cidr_block        = "10.1.1.0/24"
-  availability_zone = "us-east-2b"
+  availability_zone = "us-east-1a"
   tags = {
-    Name = "demo-public-subent"
+    Name = "demo-public_subent_01"
   }
 }
 
 # Creating Private Subnet
-resource "aws_subnet" "private-subent" {
-  vpc_id            = aws_vpc.demo-vpc
+resource "aws_subnet" "demo-private_subent_01" {
+  vpc_id            = aws_vpc.demo-vpc.id
   cidr_block        = "10.1.2.0/24"
-  availability_zone = "us-east-2b"
+  availability_zone = "us-east-1a"
   tags = {
-    Name = "demo-private-subent"
+    Name = "demo-private_subent_01"
   }
 }
 
@@ -56,8 +55,8 @@ resource "aws_eip" "demo-eip" {
 
 # Creating NAT Gateway
 resource "aws_nat_gateway" "demo-ngw" {
-  subnet_id     = aws_subnet.demo-public-subent.id
-  allocation_id = aws_eip.demo-eip
+  subnet_id     = aws_subnet.demo-public_subent_01.id
+  allocation_id = aws_eip.demo-eip.id
 
   tags = {
     Name = "demo-ngw"
@@ -69,11 +68,11 @@ resource "aws_nat_gateway" "demo-ngw" {
 
 # Creating Public Route Table
 resource "aws_route_table" "demo-public-rt" {
-  vpc_id = aws_vpc.demo-vpc
+  vpc_id = aws_vpc.demo-vpc.id
 
   route {
     cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.demo-igw
+    gateway_id = aws_internet_gateway.demo-igw.id
   }
 
   tags = {
@@ -83,11 +82,11 @@ resource "aws_route_table" "demo-public-rt" {
 
 # Creating Private Route Table
 resource "aws_route_table" "demo-private-rt" {
-  vpc_id = aws_vpc.lbb-vpc.id
+  vpc_id = aws_vpc.demo-vpc.id
 
   route {
     cidr_block = "0.0.0.0/0"
-    gateway_id = aws_nat_gateway.demo-ngw
+    gateway_id = aws_nat_gateway.demo-ngw.id
   }
 
   tags = {
@@ -96,9 +95,9 @@ resource "aws_route_table" "demo-private-rt" {
 }
 
 # Creating Security Group
-resource "aws_security_group" "lbb-ssh-sg" {
+resource "aws_security_group" "demo-ssh-sg" {
   name   = "demo-ssh-sg"
-  vpc_id = aws_vpc.demo-vpc
+  vpc_id = aws_vpc.demo-vpc.id
 
   ingress {
     from_port   = 22
@@ -128,11 +127,11 @@ resource "aws_security_group" "lbb-ssh-sg" {
 
 # Creating Public EC2 Instance
 resource "aws_instance" "public_ec2" {
-  ami                         = "ami-0f5daaa3a7fb3378b"
+  ami                         = "ami-080e1f13689e07408"
   instance_type               = "t2.micro"
   key_name                    = "tf-test-key"
   vpc_security_group_ids      = [aws_security_group.demo-ssh-sg.id]
-  subnet_id                   = aws_subnet.demo-public-subnet
+  subnet_id                   = aws_subnet.demo-public_subent_01.id
   associate_public_ip_address = true # Ensure that the instance gets a public IP address
 
   tags = {
@@ -142,11 +141,11 @@ resource "aws_instance" "public_ec2" {
 
 # Creating Private EC2 Instance
 resource "aws_instance" "private_ec2" {
-  ami                    = "ami-0f5daaa3a7fb3378b"
+  ami                    = "ami-080e1f13689e07408"
   instance_type          = "t2.micro"
   key_name               = "tf-test-key"
   vpc_security_group_ids = [aws_security_group.demo-ssh-sg.id]
-  subnet_id              = aws_subnet.demo-public-subnet
+  subnet_id              = aws_subnet.demo-private_subent_01.id
 
   tags = {
     Name = "Private-EC2"
@@ -155,12 +154,12 @@ resource "aws_instance" "private_ec2" {
 
 # Associating Public Subnet with Public Route Table
 resource "aws_route_table_association" "demo-rta-public-subent-1" {
-  subnet_id      = aws_subnet.demo-public-subnet
-  route_table_id = aws_route_table.demo-private-rt
+  subnet_id      = aws_subnet.demo-public_subent_01.id
+  route_table_id = aws_route_table.demo-public-rt.id
 }
 
 # Associating Private Subnet with Private Route Table
 resource "aws_route_table_association" "demo-rta-private-subent-1" {
-  subnet_id      = aws_subnet.demo-public-subnet
-  route_table_id = aws_route_table.demo-private-rt
+  subnet_id      = aws_subnet.demo-private_subent_01.id
+  route_table_id = aws_route_table.demo-private-rt.id
 }
